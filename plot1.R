f1bb1450d6111088ddb02f2ea0e0a320ab547232
Andrew Crisp <crispa@slhs.org>
2015-07-07 11:51:49 -0600

    Switched to print directly to file on all four graphics.

--- plot1.R
+++ plot1.R
@@ -1,3 +1,18 @@
+################################################################################
+#
+# Author:     		Andrew Crisp
+# Filename: 			plot1.R
+# Comments:			  
+#						      This script will:
+#  					      1. Check for and acquire the dataset as necessary.
+#  					      2. Sanitize the data.
+#                 3. Build the graphic.
+#  					      4. Write the graphic to file.
+#    				      
+################################################################################
+################################################################################
+### Load dataset
+################################################################################
 if(!file.exists("household_power_consumption")){
   if(!file.exists("exdata-data-household_power_consumption.zip")){
     fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -7,6 +22,9 @@ if(!file.exists("household_power_consumption")){
   unzip("exdata-data-household_power_consumption.zip")
 }
 
+################################################################################
+### Sanitize data
+################################################################################
 household_power_consumption<-read.table(file="household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?",nrows=2075259,colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 household_power_consumption$Date <- as.Date(x = household_power_consumption$Date, format = "%d/%m/%Y")
 
@@ -14,6 +32,13 @@ daysOfInterest <- subset(household_power_consumption, Date=="2007-02-01"|Date=="
 daysOfInterest$DateTime <- paste(daysOfInterest$Date,daysOfInterest$Time)
 daysOfInterest$DateTime <- strptime(x = daysOfInterest$DateTime, format = "%Y-%m-%d %H:%M:%S")
 
+################################################################################
+### Draw graphic
+################################################################################
+png("plot1.png", width=480, height=480)
 hist(daysOfInterest$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
-dev.copy(device = png,file="plot1.png")
+
+################################################################################
+### Write to file
+################################################################################
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,3 +1,18 @@
+################################################################################
+#
+# Author:       	Andrew Crisp
+# Filename: 			plot4.R
+# Comments:			  
+#						      This script will:
+#  					      1. Check for and acquire the dataset as necessary.
+#  					      2. Sanitize the data.
+#                 3. Build the graphic directly to the png device.
+#  					      4. Write the graphic to file.
+#    				      
+################################################################################
+################################################################################
+### Load dataset
+################################################################################
 if(!file.exists("household_power_consumption")){
   if(!file.exists("exdata-data-household_power_consumption.zip")){
     fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -7,6 +22,9 @@ if(!file.exists("household_power_consumption")){
   unzip("exdata-data-household_power_consumption.zip")
 }
 
+################################################################################
+### Sanitize data
+################################################################################
 household_power_consumption<-read.table(file="household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?",nrows=2075259,colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 household_power_consumption$Date <- as.Date(x = household_power_consumption$Date, format = "%d/%m/%Y")
 
@@ -14,6 +32,9 @@ daysOfInterest <- subset(household_power_consumption, Date=="2007-02-01"|Date=="
 daysOfInterest$DateTime <- paste(daysOfInterest$Date,daysOfInterest$Time)
 daysOfInterest$DateTime <- strptime(x = daysOfInterest$DateTime, format = "%Y-%m-%d %H:%M:%S")
 
+################################################################################
+### Draw graphic to png device directly
+################################################################################
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2,2))
 
@@ -26,4 +47,7 @@ points(daysOfInterest$DateTime,daysOfInterest$Sub_metering_3, col="blue", type="
 legend("topright", col=c("black", "red", "blue"), lty=1,legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),bty = "n")
 plot(daysOfInterest$DateTime,daysOfInterest$Global_reactive_power, type="l", ylab = "Global_reactive_power", xlab = "datetime")
 
+################################################################################
+### Write to file
+################################################################################
 dev.off()
